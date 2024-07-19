2ab5b26cc6263a595fb4a2b67f47e895cf972e3b
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-07-19 10:40:58 -0700

    Add config validation (#34)

--- server/internal/config/config.go
+++ server/internal/config/config.go
@@ -16,6 +16,21 @@ type Config struct {
 	Admin   Admin  `yaml:"admin"`
 }
 
+// Validate validates the configuration.
+func (c *Config) Validate() error {
+	if c.BaseURL == "" {
+		return fmt.Errorf("baseUrl must be set")
+	}
+	if err := c.Server.validate(); err != nil {
+		return err
+	}
+	if err := c.Admin.validate(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // Server is the configuration for the external HTTPS server.
 type Server struct {
 	Port      int `yaml:"port"`
@@ -26,6 +41,27 @@ type Server struct {
 	TLS *TLS `yaml:"tls"`
 	// AllowedOriginHosts is a slice of Origin hosts that we allow in CORS preflight check.
 	AllowedOriginHosts []string `yaml:"allowedOriginHosts"`
+}
+
+func (s *Server) validate() error {
+	if s.Port <= 0 {
+		return fmt.Errorf("port must be greater than 0")
+	}
+	if s.AgentPort <= 0 {
+		return fmt.Errorf("agentPort must be greater than 0")
+	}
+
+	if err := s.Auth.validate(); err != nil {
+		return err
+	}
+
+	if s.TLS != nil {
+		if err := s.TLS.validate(); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // GetAllowedOriginHosts returns allowed Origin hosts.
@@ -42,6 +78,13 @@ type Admin struct {
 	Port int `yaml:"port"`
 }
 
+func (a *Admin) validate() error {
+	if a.Port <= 0 {
+		return fmt.Errorf("port must be greater than 0")
+	}
+	return nil
+}
+
 // Auth is the authentication configuration for the proxy.
 type Auth struct {
 	RBACServer      *RBACServerAuth `yaml:"rbacServer"`
@@ -50,9 +93,39 @@ type Auth struct {
 	CacheCleanup    time.Duration   `yaml:"cacheCleanup"`
 }
 
+func (a *Auth) validate() error {
+	if a.RBACServer == nil {
+		// Authentication is not enabled.
+		return nil
+	}
+
+	if err := a.RBACServer.validate(); err != nil {
+		return err
+	}
+
+	if err := a.OIDC.validate(); err != nil {
+		return err
+	}
+	if a.CacheExpiration <= 0 {
+		return fmt.Errorf("cacheExpiration must be greater than 0")
+	}
+	if a.CacheCleanup <= 0 {
+		return fmt.Errorf("cacheCleanup must be greater than 0")
+	}
+
+	return nil
+}
+
 // RBACServerAuth is the configuration for authentication with RBAC server.
 type RBACServerAuth struct {
 	Addr string `yaml:"addr"`
+}
+
+func (r *RBACServerAuth) validate() error {
+	if r.Addr == "" {
+		return fmt.Errorf("addr must be set")
+	}
+	return nil
 }
 
 // OIDC is the configuration for OIDC.
@@ -63,10 +136,37 @@ type OIDC struct {
 	ResolverAddr string `yaml:"resolverAddr"`
 }
 
+func (o *OIDC) validate() error {
+	if o.ClientID == "" {
+		return fmt.Errorf("clientId must be set")
+	}
+	if o.ClientSecret == "" {
+		return fmt.Errorf("clientSecret must be set")
+	}
+	if o.IssuerURL == "" {
+		return fmt.Errorf("issuerUrl must be set")
+	}
+	if o.ResolverAddr == "" {
+		return fmt.Errorf("resolverAddr must be set")
+	}
+	return nil
+}
+
 // TLS is the TLS configuration for the proxy.
 type TLS struct {
 	Key  string `yaml:"key"`
 	Cert string `yaml:"cert"`
+}
+
+// validate validates the configuration.
+func (c *TLS) validate() error {
+	if c.Key == "" {
+		return fmt.Errorf("key must be set")
+	}
+	if c.Cert == "" {
+		return fmt.Errorf("cert must be set")
+	}
+	return nil
 }
 
 // Parse parses a configuration file at the given path and returns a Config
